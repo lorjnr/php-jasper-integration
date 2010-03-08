fe613acc0ed8abb3431ae5c2ed7b2e2d9098dd5d
dominic_ast@bluewin.ch <dominic_ast@bluewin.ch@7786a432-27dc-11df-bc55-67715050aa30>
2010-03-08 09:28:16 +0000

    Sample added

---
+++ var/sample/example_autosave.php
@@ -0,0 +1,44 @@
+
+<?php
+
+  include 'classes/ExRep.php';
+
+?>
+
+<html>
+  <head>
+    <title>Jasp Example</title>
+    <script type="text/javascript">
+      function showReport(rep) {
+        window.location.href = rep;
+      }
+    </script>
+  </head>
+
+  <body>
+    <center>
+      <?php
+
+        jasper_open_mngr("./");
+
+        $rep = new ExRep();
+ 	$rep->getParameters()->setStrParam("String-Parameter");
+ 	$rep->getParameters()->setIntParam(9999);
+        if ($rep->execute()) {
+          $pdf = $rep->autosave_pdf();
+	  echo "$pdf\n";
+
+          if ($pdf != "") {
+            echo "Redirect<br>\n";
+            echo "<script type=\"text/javascript\">showReport(\"$pdf\");</script>\n";
+          } else {
+            echo "Error occured<br>\n";
+          }
+        } else {
+          echo "Error occured<br>\n";
+        }
+      ?>
+    </center>
+  </body>
+</html>
+
